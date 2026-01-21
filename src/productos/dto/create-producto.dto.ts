aa44d5a2b5ae082e41c15ef5383568c8b0445e50
Juan Sebastián Rodriguez <rolo.velosa@gmail.com>
2026-01-20 21:22:32 -0500

    Cambios en los controladores y servicios

--- src/productos/dto/create-producto.dto.ts
+++ src/productos/dto/create-producto.dto.ts
@@ -1,13 +1,12 @@
-import { Decimal } from "@prisma/client/runtime/client";
-import { Type } from "class-transformer";
-import { IsNumber, IsString, Min } from "class-validator";
+import { Type } from 'class-transformer';
+import { IsNumber, IsString, Min } from 'class-validator';
 
 export class CreateProductoDto {
-    @IsString()
-    public nombre: string;
+  @IsString()
+  public nombre: string;
 
-    @IsNumber ({maxDecimalPlaces: 10})
-    @Min(0)  
-    @Type(() => Number)
-    public precio: number;
+  @IsNumber({ maxDecimalPlaces: 10 })
+  @Min(0)
+  @Type(() => Number)
+  public precio: number;
 }
--- src/productos/productos.service.ts
+++ src/productos/productos.service.ts
@@ -1,18 +1,24 @@
-import { BadRequestException, Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpStatus,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
 import { CreateProductoDto } from './dto/create-producto.dto';
 import { UpdateProductoDto } from './dto/update-producto.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Producto } from './entities/producto.entity';
 import { Repository } from 'typeorm';
 import { PaginationDto } from 'src/common';
-
-
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductosService {
   private readonly logger = new Logger('ProductosService');
-  
-  constructor(@InjectRepository(Producto) private productoRepo: Repository<Producto>) {}
+
+  constructor(
+    @InjectRepository(Producto) private productoRepo: Repository<Producto>,
+  ) {}
 
   create(createProductoDto: CreateProductoDto) {
     const producto = this.productoRepo.create(createProductoDto);
@@ -20,56 +26,65 @@ export class ProductosService {
   }
 
   async findAll(PaginationDto: PaginationDto) {
-    const{pagina=1,limite=10} = PaginationDto;
-    const totalPaginas = await this.productoRepo.count({where:{disponible:true}});
-    const ultimaPagina = Math.ceil(totalPaginas/limite);
+    const { pagina = 1, limite = 10 } = PaginationDto;
+    const totalPaginas = await this.productoRepo.count({
+      where: { disponible: true },
+    });
+    const ultimaPagina = Math.ceil(totalPaginas / limite);
 
     return {
-      data: await this.productoRepo.find({   
+      data: await this.productoRepo.find({
         skip: (pagina - 1) * limite,
         take: limite,
-        where:{disponible:true}
-  }), 
-    meta:{
-      total: totalPaginas,
-      pagina: pagina,
-      ultimaPagina: ultimaPagina,
-    }
-  }}
+        where: { disponible: true },
+      }),
+      meta: {
+        total: totalPaginas,
+        pagina: pagina,
+        ultimaPagina: ultimaPagina,
+      },
+    };
+  }
 
   async findOne(id: number) {
-    const producto = await this.productoRepo.findOneBy({id,disponible:true});
+    const producto = await this.productoRepo.findOneBy({
+      id,
+      disponible: true,
+    });
 
-    if (!producto){
-      throw new NotFoundException(`Producto con id # ${id} no encontrado`);
+    if (!producto) {
+      throw new RpcException({
+        message: `Producto con id # ${id} no encontrado`,
+        status: HttpStatus.BAD_REQUEST,
+      });
     }
     return producto;
   }
 
   async update(id: number, updateProductoDto: UpdateProductoDto) {
-    
     await this.findOne(id);
-    
+
     const data = Object.fromEntries(
-      Object.entries(updateProductoDto).filter(([_, value]) => value !== undefined)
-    );  
+      Object.entries(updateProductoDto).filter(
+        ([_, value]) => value !== undefined,
+      ),
+    );
 
     if (Object.keys(data).length === 0) {
-      throw new BadRequestException('No se proporcionaron datos para actualizar el producto.');
+      throw new RpcException({
+        message: 'No se proporcionaron datos para actualizar el producto.',
+        status: HttpStatus.BAD_REQUEST,
+      });
     }
 
     await this.productoRepo.update(id, data);
 
-     return this.findOne(id);
-    
+    return this.findOne(id);
   }
 
   async remove(id: number) {
-    
     await this.findOne(id);
 
-    return await this.productoRepo.update(id, {disponible:false});
-
-    
+    return await this.productoRepo.update(id, { disponible: false });
   }
 }
